246fd0522efd2ab8aaf77df27d6fbe85886d0153
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-03-10 13:58:00 -0400

    eyad said why not

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { Bar } from 'vue-chartjs'
+//"https://data.cityofnewyork.us/resource/bqiq-cu78.json"
+</script>
+
+<style scoped></style>
