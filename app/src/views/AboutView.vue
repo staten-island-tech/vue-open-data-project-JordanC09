0e43a11a9b2942bb88919a3a03bb96120b5cba6d
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-03-27 13:18:49 -0400

    I am a doofus

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hate Crimes by category in the Bronx</h1>
-    <RouterLink to="/"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Back to Home</button></RouterLink>
-    <RouterLink to="/Kings"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Kings</button></RouterLink>
-    <RouterLink to="/Brooklyn"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Brooklyn</button></RouterLink>
-    <RouterLink to="/Queens"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Queens</button></RouterLink>
-    <RouterLink to="/SI"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Staten Island</button></RouterLink>
-    
-    
+    <RouterLink to="/"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Back to Home
+      </button></RouterLink
+    >
+    <RouterLink to="/Kings"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Brooklyn
+      </button></RouterLink
+    >
+    <RouterLink to="/Brooklyn"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Manhattan
+      </button></RouterLink
+    >
+    <RouterLink to="/Queens"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Queens
+      </button></RouterLink
+    >
+    <RouterLink to="/SI"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Staten Island
+      </button></RouterLink
+    >
+
     <div class="w-full max-w-md h-1000 mx-auto">
       <TheChart :chartData="chartData" :chartOptions="chartOptions" />
     </div>
-    
   </div>
 </template>
 
@@ -22,30 +40,27 @@ import TheChart from '@/components/TheChart.vue'
 //let things = []
 // Religion/Religious Practice"
 // 1
-// : 
+// :
 // "Sexual Orientation"
 // 2
-// : 
+// :
 // "Race/Color"
 // 3
-// : 
+// :
 // "Gender"
 // 4
-// : 
+// :
 // "Ethnicity/National Origin/Ancestry"
 
 const chartData = ref({
   labels: ['Religion', 'Sexual Orientation', 'Race', 'Gender', 'Ancestry'],
-  datasets: [{ 
-    data: [0, 0, 0, 0, 0],
-    backgroundColor: [
-      '#FF5733', 
-      '#33FF57', 
-      '#3357FF', 
-      '#FF33A1', 
-      '#FFB833'  
-    ],
-    hoverOffset: 4 }],
+  datasets: [
+    {
+      data: [0, 0, 0, 0, 0],
+      backgroundColor: ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFB833'],
+      hoverOffset: 4,
+    },
+  ],
 })
 
 const chartOptions = ref({
@@ -54,50 +69,50 @@ const chartOptions = ref({
 
 onMounted(async () => {
   try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/bqiq-cu78.json?$limit=2000')
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/bqiq-cu78.json?$limit=2000',
+    )
     if (response.status !== 200) {
       throw new Error(response)
     } else {
       const data = await response.json()
       console.log(data)
-      
-      
+
       let crimeCounts = [0, 0, 0, 0, 0]
 
-      
       data.forEach((crime) => {
         if (crime.county === 'BRONX') {
-          if(crime.offense_category === "Religion/Religious Practice"){
+          if (crime.offense_category === 'Religion/Religious Practice') {
             crimeCounts[0] += 1
           }
-          if(crime.offense_category === "Sexual Orientation"){
+          if (crime.offense_category === 'Sexual Orientation') {
             crimeCounts[1] += 1
           }
-          if(crime.offense_category === "Race/Color"){
+          if (crime.offense_category === 'Race/Color') {
             crimeCounts[2] += 1
           }
-          if(crime.offense_category === "Gender"){
+          if (crime.offense_category === 'Gender') {
             crimeCounts[3] += 1
           }
-          if(crime.offense_category === "Ethnicity/National Origin/Ancestry"){
+          if (crime.offense_category === 'Ethnicity/National Origin/Ancestry') {
             crimeCounts[4] += 1
           }
           //if (things.includes(crime.offense_category)){
-            //console.log("bruh")
+          //console.log("bruh")
           //}else{
-            //things.push(crime.offense_category)
+          //things.push(crime.offense_category)
           //}
         }
-        
       })
       //console.log(things)
 
-      
       chartData.value = {
         labels: ['Religion', 'Sexual Orientation', 'Race', 'Gender', 'Ancestry'],
-        datasets: [{
-          data: crimeCounts, 
-        }],
+        datasets: [
+          {
+            data: crimeCounts,
+          },
+        ],
       }
     }
   } catch (error) {
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hate Crimes across the 5 Boroughs</h1>
-    <RouterLink to="/about"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Bronx</button></RouterLink>
-    <RouterLink to="/Kings"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Kings</button></RouterLink>
-    <RouterLink to="/Brooklyn"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Brooklyn</button></RouterLink>
-    <RouterLink to="/Queens"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Queens</button></RouterLink>
-    <RouterLink to="/SI"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Staten Island</button></RouterLink>
-
+    <RouterLink to="/about"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Bronx
+      </button></RouterLink
+    >
+    <RouterLink to="/Kings"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Brooklyn
+      </button></RouterLink
+    >
+    <RouterLink to="/Brooklyn"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Manhattan
+      </button></RouterLink
+    >
+    <RouterLink to="/Queens"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Queens
+      </button></RouterLink
+    >
+    <RouterLink to="/SI"
+      ><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Staten Island
+      </button></RouterLink
+    >
 
     <div class="w-full max-w-md h-1000 mx-auto">
       <BarChart :chartData="chartData" :chartOptions="chartOptions" />
     </div>
-    
   </div>
 </template>
 
@@ -19,49 +37,45 @@
 import { ref, onMounted } from 'vue'
 import BarChart from '@/components/BarChart.vue'
 
-
 const chartData = ref({
   labels: ['Bronx', 'Kings', 'Brooklyn', 'Queens', 'Staten Island'],
-  datasets: [{ 
-    label: 'Hate Crime Data',
-    data: [0, 0, 0, 0, 0],
-    backgroundColor: [
-      '#FF5733', 
-      '#33FF57', 
-      '#3357FF', 
-      '#FF33A1', 
-      '#FFB833'  
-    ],
-    
-    borderColor: [
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)',
-      'rgb(0, 0, 0)'
-    ],
-    borderWidth: 1 }],
+  datasets: [
+    {
+      label: 'Hate Crime Data',
+      data: [0, 0, 0, 0, 0],
+      backgroundColor: ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFB833'],
+
+      borderColor: [
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+        'rgb(0, 0, 0)',
+      ],
+      borderWidth: 1,
+    },
+  ],
 })
 
 const chartOptions = ref({
   responsive: true,
   plugins: {
     legend: {
-      position: 'top', 
+      position: 'top',
       labels: {
-        color: '#333', 
+        color: '#333',
         font: {
-          size: 14, 
-          weight: 'bold', 
+          size: 14,
+          weight: 'bold',
         },
       },
     },
     tooltip: {
       callbacks: {
-        label: function(tooltipItem) {
-          return tooltipItem.label + ': ' + tooltipItem.raw + ' crimes';
+        label: function (tooltipItem) {
+          return tooltipItem.label + ': ' + tooltipItem.raw + ' crimes'
         },
       },
     },
@@ -71,22 +85,21 @@ const chartOptions = ref({
       beginAtZero: true,
     },
   },
-
 })
 
 onMounted(async () => {
   try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/bqiq-cu78.json?$limit=2000')
+    const response = await fetch(
+      'https://data.cityofnewyork.us/resource/bqiq-cu78.json?$limit=2000',
+    )
     if (response.status !== 200) {
       throw new Error(response)
     } else {
       const data = await response.json()
       console.log(data)
 
-      
       let crimeCounts = [0, 0, 0, 0, 0]
 
-      
       data.forEach((crime) => {
         if (crime.county === 'BRONX') {
           crimeCounts[0] += 1
@@ -105,33 +118,27 @@ onMounted(async () => {
         }
       })
 
-      
       chartData.value = {
         labels: ['Bronx', 'Kings', 'Brooklyn', 'Queens', 'Staten Island'],
-        datasets: [{
-          data: crimeCounts, 
-          label: 'Hate Crime Data',
-          
-          backgroundColor: [
-            '#FF5733', 
-            '#33FF57', 
-            '#3357FF', 
-            '#FF33A1', 
-            '#FFB833'  
-          ],
-          
-          borderColor: [
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)',
-            'rgb(0, 0, 0)'
-          ],
-          borderWidth: 1
-        }],
-        
+        datasets: [
+          {
+            data: crimeCounts,
+            label: 'Hate Crime Data',
+
+            backgroundColor: ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFB833'],
+
+            borderColor: [
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+              'rgb(0, 0, 0)',
+            ],
+            borderWidth: 1,
+          },
+        ],
       }
     }
   } catch (error) {
